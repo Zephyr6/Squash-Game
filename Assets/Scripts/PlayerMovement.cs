1fac5ecb8908937c34a9821b86e509b07c59d732
Carlos <murlocholmes@gmail.com>
2014-02-04 00:29:13 -0700

    basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 200F;
+    public float jumpSpeed = 50F;
+    public bool canMove = true;
+
+    private Vector2 mov;
+
+    void FixedUpdate()
+    {
+        if (canMove)
+        {
+            float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+            mov = new Vector2(h, (Input.GetKeyDown("space") ? jumpSpeed : rigidbody2D.velocity.y));
+            
+            rigidbody2D.velocity = mov;
+        }
+        else
+        {
+            rigidbody2D.velocity = Vector2.zero;
+        }
+    }
+}
