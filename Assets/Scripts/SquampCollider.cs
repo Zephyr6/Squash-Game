0b84bc6d3529f237436a816fb67dd368bdaa80c0
Carlos <murlocholmes@gmail.com>
2014-02-08 18:10:56 -0700

    Squamp Collider fixes

--- Assets/Scripts/SquampCollider.cs
+++ Assets/Scripts/SquampCollider.cs
@@ -5,11 +5,16 @@ public class SquampCollider : MonoBehaviour {
 
     public AudioClip clip = new AudioClip();
     public float TimeToSquamp = 5.0f;
+
+    private bool isFalling = false;
     private float timer = 0;
-    private Vector3 startPosition = new Vector3();
+    private Vector3 startPosition;
     private float fallSpeed = 10;
+
+
 	// Use this for initialization
 	void Start () {
+        startPosition = transform.position;
         timer = TimeToSquamp;
 	}
 	
@@ -18,6 +23,7 @@ public class SquampCollider : MonoBehaviour {
         if (timer <= 0)
         {
             rigidbody2D.velocity = new Vector2(0, -fallSpeed);
+            isFalling = true;
             timer = TimeToSquamp;
         }
         else
@@ -25,9 +31,18 @@ public class SquampCollider : MonoBehaviour {
             timer -= Time.deltaTime;
         }
 
+        if (transform.position.y >= startPosition.y)
+        {
+            if (!isFalling)
+            {
+                rigidbody2D.velocity = Vector2.zero;
+            }
+        }
+
         if (GetComponentInChildren<SquampGrounded>().IsGrounded)
         {
-            rigidbody2D.velocity = new Vector2(0, fallSpeed);
+            isFalling = false;
+            rigidbody2D.velocity = new Vector2(0, fallSpeed * 0.3F);
         }
 
         rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
