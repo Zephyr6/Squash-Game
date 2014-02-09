4f4fb2309bdbab717117ec01d090c5a55fcb43a7
truffles23 <wailingshadow@gmail.com>
2014-02-09 13:34:02 -0700

    exit level script

---
+++ Assets/Scripts/ExitLevel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitLevel : MonoBehaviour {
+
+    public int NextLevel = 0;
+    public AudioClip doorOpen = new AudioClip();
+    public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            var coinCollector = other.GetComponent<CoinCollector>();
+            if (coinCollector.AllCollected())
+            {
+                AudioSource.PlayClipAtPoint(doorOpen, transform.position);
+                Destroy(door);
+                Application.LoadLevel(NextLevel);
+            }
+        }
+    }
+}
