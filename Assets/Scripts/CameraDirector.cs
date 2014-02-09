9ae36e801127c9260054d8eb872522a9b2c7b4bd
truffles23 <wailingshadow@gmail.com>
2014-02-08 20:18:28 -0700

    made changes to player prefab and created camera prefab and director script

---
+++ Assets/Scripts/CameraDirector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDirector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if(GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            var playerObject = GameObject.FindGameObjectWithTag("Player");
+            transform.position.Set(playerObject.transform.position.x, playerObject.transform.position.y, transform.position.z);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            var playerObject = GameObject.FindGameObjectWithTag("Player");
+            transform.position.Set(playerObject.transform.position.x, playerObject.transform.position.y, transform.position.z);
+        }
+	}
+}
