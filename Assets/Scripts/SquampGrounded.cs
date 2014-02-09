d8e1aa0c1068fec406bff4e39e0b9e08f1aab80e
truffles23 <wailingshadow@gmail.com>
2014-02-08 17:39:10 -0700

    see this script looks like shit

---
+++ Assets/Scripts/SquampGrounded.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquampGrounded : MonoBehaviour {
+    public Transform groundCheck;
+    public bool IsGrounded = false;
+    public float TimeToSquamp = 5;
+    private float timer = 0;
+    private Vector3 startPosition = new Vector3();
+    private float fallSpeed = 100;
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        IsGrounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+    }
+}
